cc8cf0d0a7fde1142f9f922a8a3e84c78607c2e9
Michael Peters <michaelpeterswa@icloud.com>
2021-11-30 21:24:21 -0800

    feat: day1 complete

--- days/day1/day1.go
+++ days/day1/day1.go
@@ -17,17 +17,72 @@
 package day1
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"strconv"
 )
 
-func Day1(filename string){
+type Depths struct {
+	Measurements []int64
+}
+
+func Day1(filename string) {
 	fmt.Printf("AoC Day1 2021\n\n")
 
-	data, err := os.ReadFile(filename)
+	var depths Depths
+
+	file, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(data))
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, _, err := reader.ReadLine()
+
+		if err == io.EOF {
+			break
+		}
+
+		depth, err := strconv.ParseInt(string(line), 10, 64)
+		if err != nil {
+			log.Fatal("failed to parse integer")
+		}
+		depths.Measurements = append(depths.Measurements, depth)
+	}
+
+	// PART ONE
+
+	var partOneTotal int64
+	depthsLen := len(depths.Measurements)
+	for k, depth := range depths.Measurements {
+		if k < depthsLen-1 {
+			if depth < depths.Measurements[k+1] {
+				partOneTotal += 1
+			}
+		}
+	}
+
+	fmt.Printf("Total Depth Increases (Part 1): %d\n", partOneTotal)
+
+	// PART TWO
+
+	var partTwoTotal int64
+	for k := range depths.Measurements {
+		if k < depthsLen-3 {
+			firstSum := depths.Measurements[k] + depths.Measurements[k+1] + depths.Measurements[k+2]
+			secondSum := depths.Measurements[k+1] + depths.Measurements[k+2] + depths.Measurements[k+3]
+			if firstSum < secondSum {
+				partTwoTotal += 1
+			}
+		}
+	}
+
+	fmt.Printf("Total Depth Increases (Part 2): %d\n", partTwoTotal)
 }
--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "github.com/michaelpeterswa/AdventOfCode2021/days/day1"
 
 func main() {
+	One()
+}
+
+func One() {
 	day1.Day1("days/day1/day1.txt")
 }
